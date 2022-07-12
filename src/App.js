2e64648cd8077ba92571c6459f621f8e18251fef
Irene Carrion Retamero <105889766+IreneCar@users.noreply.github.com>
2022-07-12 20:19:53 +0200

    Iteration 1-almost 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
+import {useState} from "react";
+import Contacts from "./contacts.json";
+
+
+
+
 
 function App() {
+  let fiveContacts = Contacts.slice(0,5)
+  const [contacts, setContacts] = useState(fiveContacts);
+  const [count, setCount] = useState(0);
+
+  function random() {
+    let randomIndex = Math.floor(Math.random() * Contacts.length)
+    setContacts([...contacts, Contacts[randomIndex]]);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <h2>IronContacts</h2>
+    <button onClick={random}>Add Random Contact</button>
+    <table>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+        <th>Won an Oscar</th>
+        <th>Won an Emmy</th>
+      </tr>
+    
+        {fiveContacts.map((contact) => {
+          return (
+          <tr>
+            <td>
+              <img src={contact.pictureUrl} alt="actor"></img>
+            </td>
+            <td>
+              <p>{contact.name}</p>
+            </td>
+            <td>
+              <p>{contact.popularity.toFixed(2)}</p>
+            </td>
+            <td>
+              {contact.wonOscar && <p>🏆</p>}
+            </td>
+            <td>
+              {contact.wonEmmy && <p>🏆</p>}
+            </td>
+          </tr>
+          );        
+        })}
+      
+    </table>
+  
+  </div>
   );
 }
-
 export default App;
